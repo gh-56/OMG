f82d8f9ca7299b879a1808ac0b82fdd898235643
song7446 <songwon199943@gmail.com>
2023-09-11 16:20:06 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './component/Home'
 
 function App() {
   return (
     <div className='App'>
-      <p>hello2</p>
+      <Home />
     </div>
   );
 }
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="Header">영화 맞추기 게임</h1>
+      <section>
+        <input type="text" placeholder='정답을 입력하세요'/>
+        <button>제출</button>
+      </section>
+    </div>)
 }
 
 export default Home;
