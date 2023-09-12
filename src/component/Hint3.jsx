a0b9074157eb8bde25accfbeffe48830577a115d
gh-56 <dinggul56@gmail.com>
2023-09-12 14:01:08 +0900

    세 번재 힌트(포스터 사진) 구현

--- src/component/Hint3.jsx
+++ src/component/Hint3.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function Hint3() {
-  return <div>Hint3</div>;
+function Hint3(props) {
+  const img_url = `${props.img}${props.img_size}${props.poster_path}`;
+
+  return (
+    <div>
+      <h3>세 번째 힌트</h3>
+      <img src={img_url} alt='사진 안뜸' />
+    </div>
+  );
 }
 
 export default Hint3;
--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -7,25 +7,34 @@ import { useState, useEffect } from 'react';
 function Movie() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState(null);
+  const [img, setImg] = useState(null);
 
   const getMovie = async () => {
     setLoading(true);
     const queryString =
       '&page=1&include_adult=false&include_video=false&language=ko-KR&sort_by=popularity.desc';
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=f5217a0db9120b09b842b37fe18c9685${queryString}`;
+    const api = 'api_key=f5217a0db9120b09b842b37fe18c9685';
+    const url = `https://api.themoviedb.org/3/discover/movie?${api}${queryString}`;
+    const url2 = `https://api.themoviedb.org/3/configuration?${api}`;
 
     const response = await (await fetch(url)).json();
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-    const idx = 10;
+    const response2 = await (await fetch(url2)).json();
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then((json) => console.log(json));
+    // fetch(url2)
+    //   .then((res) => res.json())
+    //   .then((json) => console.log(json));
+    const idx = 7;
     setMovies(response.results[idx]);
+    setImg(response2.images);
     setLoading(false);
   };
   useEffect(() => {
     getMovie();
+    setImg();
   }, []);
-  console.log(movies);
+  // console.log(movies);
   return (
     <div>
       {loading ? (
@@ -37,10 +46,14 @@ function Movie() {
             title={movies.title}
             overview={movies.overview}
           ></Hint30>
+          <Hint15 />
+          <Hint3
+            poster_path={movies.poster_path}
+            img={img.base_url}
+            img_size={img.poster_sizes[2]}
+          ></Hint3>
         </div>
       )}
-      <Hint15 />
-      <Hint3 />
     </div>
   );
 }
