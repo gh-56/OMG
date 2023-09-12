1d9ebb46a7e83514657f76811d7e238b09d5673c
gh-56 <dinggul56@gmail.com>
2023-09-12 12:01:38 +0900

    영화 줄거리들 중 하나만 불러오기

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hint30 from './Hint30';
 import Hint15 from './Hint15';
 import Hint3 from './Hint3';
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function Movie() {
@@ -10,15 +9,17 @@ function Movie() {
   const [movies, setMovies] = useState(null);
 
   const getMovie = async () => {
+    setLoading(true);
     const queryString =
-      '&page=1&include_adult=false&include_video=false&language=kr&sort_by=popularity.desc';
+      '&page=1&include_adult=false&include_video=false&language=ko-KR&sort_by=popularity.desc';
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=f5217a0db9120b09b842b37fe18c9685${queryString}`;
 
     const response = await (await fetch(url)).json();
-    // fetch(url)
-    //   .then((res) => res.json())
-    //   .then((json) => console.log(json));
-    setMovies(response.results);
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+    const idx = 10;
+    setMovies(response.results[idx]);
     setLoading(false);
   };
   useEffect(() => {
@@ -31,9 +32,11 @@ function Movie() {
         <div>로딩중입니다.</div>
       ) : (
         <div>
-          {movies.map((movie) => (
-            <Hint30 key={movie.id} title={movie.title}></Hint30>
-          ))}
+          <Hint30
+            key={movies.id}
+            title={movies.title}
+            overview={movies.overview}
+          ></Hint30>
         </div>
       )}
       <Hint15 />
