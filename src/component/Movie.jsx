ad9c576e95f8a4a8bf74aba4d8f89f85898303c1
gh-56 <dinggul56@gmail.com>
2023-09-14 11:37:41 +0900

    랜덤 영화 추출 & 예외 처리

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -3,14 +3,12 @@ import Hint30 from './Hint30';
 import Hint15 from './Hint15';
 import Hint3 from './Hint3';
 import { useState, useEffect } from 'react';
-
 function Movie() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState(null);
   const [posterImg, setPosterImg] = useState(null);
   const [count, setCount] = useState(10);
   const [keyword, setKeyword] = useState(0);
-
   const getMovie = async () => {
     setLoading(true);
     const queryString =
@@ -18,34 +16,40 @@ function Movie() {
     const api = '?api_key=f5217a0db9120b09b842b37fe18c9685';
     const url = `https://api.themoviedb.org/3/discover/movie${api}${queryString}`;
     const url_posterPath = `https://api.themoviedb.org/3/configuration${api}`;
-
-    const response = await (await fetch(url)).json();
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-    const response2 = await (await fetch(url_posterPath)).json();
-    fetch(url_posterPath)
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-
-    const idx = 5;
-    setMovies(response.results[idx]);
-    setPosterImg(response2.images);
-
-    // 키워드
-    const url_keywords = `https://api.themoviedb.org/3/movie/${response.results[idx].id}/keywords${api}`;
-    const response3 = await (await fetch(url_keywords)).json();
-    fetch(url_keywords)
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-    setKeyword(response3.keywords[0]);
-
-    setLoading(false);
+    while (true) {
+      const response = await (await fetch(url)).json();
+      const idx = Math.floor(Math.random() * response.results.length);
+      console.log('idx = ' + idx);
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+      const response2 = await (await fetch(url_posterPath)).json();
+      fetch(url_posterPath)
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+      setPosterImg(response2.images);
+      // 키워드
+      const url_keywords = `https://api.themoviedb.org/3/movie/${response.results[idx].id}/keywords${api}`;
+      const response3 = await (await fetch(url_keywords)).json();
+      fetch(url_keywords)
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+      if (
+        response.results[idx].overview === '' ||
+        response3.keywords.length === 0
+      ) {
+        continue;
+      } else {
+        setMovies(response.results[idx]);
+        setKeyword(response3.keywords[0]);
+        setLoading(false);
+        break;
+      }
+    }
   };
   useEffect(() => {
     getMovie();
   }, []);
-
   useEffect(() => {
     count > 0 && setTimeout(() => setCount(count - 1), 1000);
   }, [count]);
@@ -61,9 +65,7 @@ function Movie() {
             title={movies.title}
             overview={movies.overview}
           ></Hint30>
-
           <Hint15 count={count} name={keyword.name}></Hint15>
-
           <Hint3
             count={count}
             img={posterImg.base_url}
@@ -75,5 +77,4 @@ function Movie() {
     </div>
   );
 }
-
 export default Movie;
