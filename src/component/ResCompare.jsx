9d8ef590f752a4adaecd53db82c7eb08ccbdc076
gh-56 <dinggul56@gmail.com>
2023-09-18 14:13:07 +0900

    첫 화면 돌아가기 구현

--- src/component/ResCompare.jsx
+++ src/component/ResCompare.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { movieContext } from './Movie';
 import './ResCompare.css';
 function ResCompare() {
-  const { movies, setIsTrue, setAnswer, setGameCount } =
+  const { movies, setIsTrue, setAnswer, setGameCount, isTrue } =
     useContext(movieContext);
   const [inputText, setInputText] = useState('');
   const onChangeHandler = (event) => {
@@ -19,14 +19,31 @@ function ResCompare() {
       console.log('오답');
     }
   };
+  const onClickAlter = () => {
+    alert('이미 제출하셨습니다.');
+  };
   return (
     <div className='ResCompare'>
-      <input
-        value={inputText}
-        onChange={onChangeHandler}
-        placeholder='정답을 입력해주세요'
-      />
-      <button onClick={onClickHandler}>제출</button>
+      {isTrue ? (
+        <div>
+          <input
+            value={inputText}
+            onChange={onChangeHandler}
+            placeholder='정답을 입력해주세요'
+            disabled
+          />
+          <button onClick={onClickAlter}>제출</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            value={inputText}
+            onChange={onChangeHandler}
+            placeholder='정답을 입력해주세요'
+          />
+          <button onClick={onClickHandler}>제출</button>
+        </div>
+      )}
       <div>{movies.title}</div>
     </div>
   );
